5a6e4e25ebe407028d328ed5d3f366649a0df4e8
Darío <dariiodallara@gmail.com>
2021-07-05 14:44:37 -0300

    Dockerización de frontent y vista de prueba inicial

---
+++ frontend/src/components/MyMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-jumbotron
+      header="Federación de Mutuales"
+      lead="Los mejores beneficios para vos"
+    >
+    <p>¿Que onda chango todo bien? ahre</p>
+    <b-button>Botoncito de prueba</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <my-menu></my-menu>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MyMenu from '../components/MyMenu.vue';
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    MyMenu
   },
 };
 </script>
