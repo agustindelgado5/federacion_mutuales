ede9bdaa911de56bc84aaa0f8319b43de88fb7c9
juancastelli1 <juancastelli12345@gmail.com>
2021-08-11 17:09:26 -0300

    menu completo y funcional antes de la separacion

---
+++ frontend/src/infomenu.js
@@ -0,0 +1,70 @@
+
+const data = {
+    title: 'Federacion de Mutuales',
+    children: [
+        {
+            title: `Inicio`,
+            link: `/`,
+            children: [],
+        },
+        {
+            title: `Socios`,
+            link: '/socios',
+            children: [],
+        },
+        {
+            title: `Profesionales`,
+            link: '/profesionales',
+            children: [
+                {
+                    title: `Prueba 1`,
+                    children: [
+                        {
+                            title: `Prueba 1.1`,
+                            link: `/`,
+                            children: [],
+                        },
+                        {
+                            title: `Prueba 1.2`,
+                            link: `/`,
+                            children: [],
+                        },
+                        {
+                            title: `Prueba 1.3`,
+                            link: `/`,
+                            children: [],
+                        },
+                    ],
+                },
+            ],
+         
+        },
+        {
+            title: `Mutuales`,
+            link: '/mutuales',
+            children: [],
+        },
+        {
+            title: `Farmacias`,
+            link: '/farmacias',
+            children: [],
+        },
+        {
+            title: `Medicamentos`,
+            link: '/medicamentos',
+            children: [],
+        },
+        {
+            title: `Recetas`,
+            link: '/recetas',
+            children: [],
+        },
+        {
+            title: `Ordenes`,
+            link: '/ordenes',
+            children: [],
+        },
+    ],
+};
+
+export default data;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,22 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueSidebarMenu from 'vue-sidebar-menu'
+
+Vue.use(VueSidebarMenu)
 
 Vue.config.productionTip = false;
+
+export default {
+    install(Vue, options) {
+        Vue.component('vue-nested-menu', App);
+    },
+};
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
+
