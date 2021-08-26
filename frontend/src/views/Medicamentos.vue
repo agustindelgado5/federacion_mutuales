8c16cba4dd167cc78fc9728f77fea5282e7dc34a
Leonel-H29 <leonelherreradkno@gmail.com>
2021-08-25 22:12:21 -0300

    Termino la funcion eliminar de la tabla Medicamentos de forma exitosa y el buscador de la tabla

--- frontend/src/views/Medicamentos.vue
+++ frontend/src/views/Medicamentos.vue
@@ -21,6 +21,34 @@
       <template #modal-title><h5 class="modal-title">Alta</h5></template>
       <medicamento-alta/>
     </b-modal>
+
+    <!-- ======== Formulario de Busqueda ======== -->
+    <div>
+      <b-input-group size="sm" class="mb-2">
+        <b-input-group-prepend is-text>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+            />
+          </svg>
+        </b-input-group-prepend>
+        <b-form-input
+          v-model="buscar"
+          type="text"
+          placeholder="Busque un registro"
+          v-on:keyup="buscarnow()" 
+          ref="buscadorlista"
+        ></b-form-input>
+      </b-input-group>
+    </div>
+    <!-- ======================================== -->
     
     <b-table
       :fields="fields"
@@ -32,6 +60,8 @@
       show-empty
       :sticky-header= true
       :no-border-collapse= false
+      ref="tablaregistros"
+      id="tablaregistros"
     >
     <!-- 
       <template slot="action">
@@ -47,17 +77,19 @@
         {{data.value.split('/')[4]}}
       </template>
       
-      <template slot="cell(action)" slot-scope="">
+      <template slot="cell(action)" slot-scope="row">
         <div class="mt-3">
           <b-button-group>
-            <b-button variant="info" id="button-1" title="Mostrar Info"
-              >Mostrar Info</b-button
-            >
+            <b-button variant="info" id="button-1" title="Mostrar Info" @click="info(row.item.id_medicamento)">
+              Mostrar Info
+            </b-button>
 
             <b-button
               variant="warning"
               id="button-2"
               title="Editar este registro"
+              v-b-modal.modal-editar 
+              @click="editarMedicamento()"
             >
               <v-icon class="mr-2">
                 mdi-pencil
@@ -65,10 +97,15 @@
               Editar
             </b-button>
 
+            <b-modal id="modal-editar" hide-footer> 
+              <template #modal-title><h5 class="modal-title">Editar</h5></template>
+                <medicamento-update/>
+            </b-modal>
+
             <b-button
               variant="danger"
               id="button-3"
-              @click="showModal"
+              @click="showModalinfo(row.item, row.index)"
               title="Eliminar este registro"
             >
               <v-icon class="mr-2">
@@ -78,24 +115,29 @@
             </b-button>
             
 
-            <b-modal ref="my-modal" hide-footer title="Eliminar">
+            <b-modal id="modal_eliminar" ref="my-modal" hide-footer title="Eliminar" ok-only>
               <div class="d-block text-center">
-                <h3>¿Esta seguro de eliminar los datos de ... ?</h3>
+                <h3>¿Esta seguro de eliminar los datos de '{{infoEliminar.medicamento.nombre}}'?</h3>
               </div>
               <b-button
                 class="mt-2"
                 block
                 @click="hideModal"
                 title="Volver Atras"
-                >Volver Atras</b-button
               >
+                Volver Atras
+              </b-button>
+
               <b-button
                 class="mt-3"
                 variant="danger"
                 block
                 title="Eliminar"
-                >Eliminar</b-button
+                @click="deleteMedicamento(infoEliminar.medicamento.id_medicamento)"
               >
+                Eliminar
+              </b-button>
+
             </b-modal>
           </b-button-group>
         </div>
@@ -111,8 +153,11 @@ api.pathname = "medicamentos";
 api.port = 8081;
 
 import MedicamentoAlta from './MedicamentosAlta.vue';
+import MedicamentoUpdate from './MedicamentosUpdate.vue';
+import axios from 'axios';
+
 export default {
-  components: { MedicamentoAlta },
+  components: { MedicamentoAlta, MedicamentoUpdate },
   data() {
     return {
       tabla_med: [],
@@ -123,7 +168,13 @@ export default {
             {key:'laboratorio' ,label: 'Laboratotio', sortable: true,},
             {key:'cod_farmacia' ,label: 'Farmacia',sortable: true,},
             { key: "action", label: "Acciones", variant: "secondary" },
-        ],
+      ],
+      buscar: '',
+      infoEliminar:{
+        id:"modal_eliminar",
+        medicamento: -1
+      },
+
     };
   },
   methods: {
@@ -149,7 +200,58 @@ export default {
     hideModal() {
       this.$refs["my-modal"].hide();
     },
+
+    info(id){
+      alert('id: ' + id);
+    },
+
+    showModalinfo(item, index) {
+      this.infoEliminar.medicamento=item;
+      this.showModal();
+    },
+
     altaMedicamento() {},
+
+    editarMedicamento() {},
+
+    async deleteMedicamento(id){
+    
+     axios.delete('http://localhost:8081/medicamentos/'+ id +'/')
+     .then(datos =>{
+       swal("Carga Exitosa", " ", "success");
+       console.log(datos);
+     })
+     .catch(error=>{
+       swal("¡ERROR!", "Se ha detectado un problema ", "error")
+       console.log(error);
+     })
+     .finally(() => this.testFetch());
+    },
+
+    async buscarnow() {
+        // Declare variables
+        
+        var input, filter, table, tr, td, i, txtValue, IdMed, Medicamento, Laboratorio, Farmacia;
+        input = this.$refs.buscadorlista;
+        filter = input.value.toUpperCase();
+        table = document.getElementById('tablaregistros');
+        tr = table.getElementsByTagName('tr');
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 1; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            IdMed = td[0].textContent || td[0].innerText;
+            Medicamento = td[1].textContent || td[1].innerText;
+            Laboratorio = td[3].textContent || td[3].innerText;
+            Farmacia = td[4].textContent || td[4].innerText;
+            txtValue = IdMed + Medicamento + Laboratorio + Farmacia;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    },
   },
   
 };
