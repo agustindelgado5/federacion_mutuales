216416d09be86279b526bbaad83aa25d3546bac8
juancastelli1 <juancastelli12345@gmail.com>
2021-09-06 12:33:18 -0300

    menu ordenado

--- frontend/src/infomenu.js
+++ frontend/src/infomenu.js
@@ -8,6 +8,11 @@ const data = {
             children: [],
         },
         {
+            title: `Usuarios`,
+            link: `/usuarios`,
+            children: [],
+        },
+        {
             title: `Socios`,
             link: '/socios',
             children: [],
@@ -40,28 +45,98 @@ const data = {
          
         },
         {
+            title: `Cobradores`,
+            link: '/cobradores',
+            children: [],
+        },
+        {
+            title: `Salud`,
+            link: '/salud',
+            children: [
+                {
+                    title: `Ordenes Medicas`,
+                    link: '/ordenesmedicas',
+                    children: [],
+                },
+                {
+                    title: `Institutos`,
+                    link: '/institutos',
+                    children: [],
+                },
+                {
+                    title: `Estudios`,
+                    link: '/estudios',
+                    children: [
+                        {
+                            title: `Imagenes`,
+                            link: `/`,
+                            children: [],
+                        },
+                        {
+                            title: `Analisis Bioquimicos`,
+                            link: `/`,
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    title: `Cirugias`,
+                    link: '/cirugias',
+                    children: [],
+                },
+            ],
+
+        },
+        {
+            title: `Farmacias`,
+            link: '/farmacias',
+            children: [
+                {
+                    title: `Info Farmacias`,
+                    link: `/farmacias`,
+                    children: [],
+                },
+                {
+                    title: `Medicamentos`,
+                    link: `/medicamentos`,
+                    children: [],
+                },
+                {
+                    title: `Recetas`,
+                    link: `/recetas`,
+                    children: [],
+                },
+            ],
+        },
+        {
+            title: `Registro Contable`,
+            link: '/registrocontable',
+            children: [
+                {
+                    title: `Gastos Salientes`,
+                    link: `/salientes`,
+                    children: [],
+                },
+                {
+                    title: `Gastos Profesionales`,
+                    link: `/profesionales`,
+                    children: [],
+                },
+                {
+                    title: `Cuotas de Socios`,
+                    link: `/cuotas`,
+                    children: [],
+                },
+            ],
+        },
+        {
             title: `Mutuales`,
             link: '/mutuales',
             children: [],
         },
         {
-            title: `Farmacias`,
-            link: '/farmacias',
-            children: [],
-        },
-        {
-            title: `Medicamentos`,
-            link: '/medicamentos',
-            children: [],
-        },
-        {
-            title: `Recetas`,
-            link: '/recetas',
-            children: [],
-        },
-        {
-            title: `Ordenes`,
-            link: '/ordenes',
+            title: `Servicios`,
+            link: '/servicios',
             children: [],
         },
     ],
