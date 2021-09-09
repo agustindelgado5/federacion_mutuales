242dd28d1497bcefca79c04652aa54266a1ac2ac
Leonel-H29 <leonelherreradkno@gmail.com>
2021-09-09 10:23:52 -0300

    Comienzo a implementar los usuarios en el backend

--- backend/Administracion/models.py
+++ backend/Administracion/models.py
@@ -1,3 +1,11 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# # Create your models here.
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=150, unique=True)
+    """
+    class Meta (AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+    """
