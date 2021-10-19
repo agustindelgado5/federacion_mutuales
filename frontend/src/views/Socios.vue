6de292e5fcd387e578e7a36726527de1efd7c9a5
Leonel-H29 <leonelherreradkno@gmail.com>
2021-10-18 21:07:32 -0300

    Agrego el carnet del socio + cambio form de busqueda

--- frontend/src/views/Socios.vue
+++ frontend/src/views/Socios.vue
@@ -88,31 +88,26 @@
     </div>
 
     <!-- ======== Formulario de Busqueda ======== -->
-    <div>
-      <b-input-group size="sm" class="mb-2">
-        <b-input-group-prepend is-text>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-search"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-            />
-          </svg>
-        </b-input-group-prepend>
+    <b-form-group
+      label-for="filter-input"
+      label-align-sm="right"
+      label-size="sm"
+      class="mb-0"
+      style="width:100%; padding-bottom:1em;"
+    >
+      <b-input-group size="sm">
         <b-form-input
-          v-model="buscar"
-          type="text"
-          placeholder="Busque un registro"
-          v-on:keyup="buscarnow()"
-          ref="buscadorlista"
-        ></b-form-input>
+          id="filter-input"
+          v-model="filter"
+          type="search"
+          placeholder="Buscar registros"
+      ></b-form-input>
+
+        <b-input-group-append>
+          <b-button :disabled="!filter" @click="filter = ''">Limpiar</b-button>
+        </b-input-group-append>
       </b-input-group>
-    </div>
+    </b-form-group>
     <!-- ======================================== -->
 
     <div v-if="rows > 0">
@@ -166,6 +161,8 @@
       selectable
       select-mode="multi"
       @row-selected="seleccionar_una"
+      :filter="filter"
+      @filtered="onFiltered"
     >
       <template #empty="">
         <b>No hay registros para mostrar</b>
@@ -194,29 +191,19 @@
         {{ data.value.toUpperCase() }}
       </template>
 
+      <template slot="cell(fecha_nacimiento)" slot-scope="data">
+        {{data.value.split('-')[2]}}/{{data.value.split('-')[1]}}/{{data.value.split('-')[0]}}
+      </template>
+
+      <template slot="cell(carencia)" slot-scope="data">
+        {{data.value.split('-')[2]}}/{{data.value.split('-')[1]}}/{{data.value.split('-')[0]}}
+      </template>
+
       <template slot="cell(departamento)" slot-scope="data">
         {{ data.value.toUpperCase() }}
       </template>
 
-      <!--Libreria Luxon para formatear las fechas, no esta del todo bien aun XD
-      -->
-      <!-- 
-      <template slot="cell(fecha_nacimiento)" slot-scope="data">
-        {{data.value|luxon}}
-      </template>
-
-      <template slot="cell(carencia)" slot-scope="data">
-        {{data.value|luxon}}
-      </template>
-      -->
-
-      <template slot="cell(apellido)" slot-scope="row">
-        {{ row.value.toUpperCase() }}
-      </template>
-
-      <template slot="cell(nombre)" slot-scope="row">
-        {{ row.value.toUpperCase() }}
-      </template>
+      
 
       <template slot="cell(action)" slot-scope="row">
         <div class="mt-3">
@@ -378,9 +365,38 @@
               </div>
             </b-list-group>
           </div>
+
+          <b-button
+            @click="generarCarnet(row.item)"  
+            id="btn_down_pdf"
+            class="mb-0 ml-2"
+            title="Generar PDF"
+            variant="info"
+            style="color: white"
+          >
+              
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-file-pdf-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M5.523 10.424c.14-.082.293-.162.459-.238a7.878 7.878 0 0 1-.45.606c-.28.337-.498.516-.635.572a.266.266 0 0 1-.035.012.282.282 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548zm2.455-1.647c-.119.025-.237.05-.356.078a21.035 21.035 0 0 0 .5-1.05 11.96 11.96 0 0 0 .51.858c-.217.032-.436.07-.654.114zm2.525.939a3.888 3.888 0 0 1-.435-.41c.228.005.434.022.612.054.317.057.466.147.518.209a.095.095 0 0 1 .026.064.436.436 0 0 1-.06.2.307.307 0 0 1-.094.124.107.107 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256zM8.278 4.97c-.04.244-.108.524-.2.829a4.86 4.86 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.517.517 0 0 1 .145-.04c.013.03.028.092.032.198.005.122-.007.277-.038.465z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm.165 11.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.64 11.64 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.856.856 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.844.844 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.76 5.76 0 0 0-1.335-.05 10.954 10.954 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.238 1.238 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a19.707 19.707 0 0 1-1.062 2.227 7.662 7.662 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103z"
+              />
+            </svg>
+          </b-button>
         </b-card>
+        
       </template>
     </b-table>
+    
 
     <b-container fluid>
       <b-col class="my-1">
@@ -388,7 +404,7 @@
           v-model="currentPage"
           align="center"
           pills
-          :total-rows="rows"
+          :total-rows="totalRows"
           :per-page="perPage"
           aria-controls="table_socios"
         >
@@ -399,6 +415,73 @@
       <template #modal-title><h5 class="modal-title">Editar</h5></template>
       <socios-update :socio="editar" />
     </b-modal>
+
+    <!-- ==================================CREAR CARNET================================== -->
+    <vue-html2pdf
+      :show-layout="false"
+      :float-layout="true"
+      :enable-download="false"
+      :preview-modal="true"
+      :paginate-elements-by-height="1400"
+      filename="DetalleOrden"
+      :pdf-quality="2"
+      :manual-pagination="false"
+      pdf-format="a4"
+      pdf-orientation="portrait"
+      pdf-content-width="80%"
+      @progress="onProgress($event)"
+      @startPagination="startPagination()"
+      @hasPaginated="hasPaginated()"
+      @beforeDownload="beforeDownload($event)"
+      @hasDownloaded="hasDownloaded($event)"
+      ref="html2Pdf"
+    >
+      <section slot="pdf-content">
+        <!-- PDF Content Here -->
+        <section class="pdf-item">
+          <b-card-group deck>
+            <b-card img-src="../assets/logo.jpg" img-alt="Logo Federación" img-left class="mb-3" img-width="90" img-height="90">
+              <b-card-text>
+                <h6>Federación Tucumana de Mutuales</h6>
+                <div>
+                  <b-list-group horizontal="md" style="font-size: 70%">
+                    <b-list-group-item>
+                      <b>N°: {{carnetAPDF.numero_socio}}</b> | <b>Afiliado:</b> {{carnetAPDF.afiliado}} | <b>DNI:</b> {{carnetAPDF.dni}}
+                      {{carnetAPDF.adherentes}}
+                    </b-list-group-item>
+                  </b-list-group>
+                  <b-list-group>
+                    &nbsp;
+                    <h6>Adherentes</h6>
+                    
+                    <b-list-group-item
+                      v-for="adh in carnetAPDF.adhrentes"
+                      :key="adh.dni"
+                      style="font-size: 70%"
+                    >
+                      <b>{{adh.dni}}</b> - {{adh.adherente}}
+                    </b-list-group-item>
+                  </b-list-group>
+                </div>
+              </b-card-text>
+            </b-card>
+          </b-card-group>
+        </section>
+        <!--
+        <section class="pdf-item">
+          <h3>Orden Médica</h3>
+
+          <b-list-group>
+            <b-list-group-item
+              v-for="value in fields.slice(0, -1)"
+              :key="value.key"
+              >{{ value.label }}: {{ ordenAPDF[value.key] }}</b-list-group-item
+            >
+          </b-list-group>
+        </section>
+        -->
+      </section>
+    </vue-html2pdf>
   </div>
 </template>
 
@@ -412,13 +495,14 @@ api.port = 8081;
 import VueAwesomplete from "vue-awesomplete";
 import SociosAlta from "./SociosAlta.vue";
 import SociosUpdate from "./SociosUpdate.vue";
+import VueHtml2pdf from "vue-html2pdf";
 
 import axios from "axios";
 import { APIControler } from "../store/APIControler";
 
 
 export default {
-  components: { SociosAlta,SociosUpdate,VueAwesomplete },
+  components: { SociosAlta,SociosUpdate,VueAwesomplete, VueHtml2pdf },
   data() {
     return {
       tabla_socios: [],
@@ -444,6 +528,7 @@ export default {
         {key: "aldia", label: "Al Día", sortable:true},
         { key: "action", label: "Acciones", variant: "secondary" },
       ],
+      filter: null,
       totalRows: 1, //Total de filas
       currentPage: 1, //Pagina actual
       perPage: 20, // Datos en la tabla por pagina
@@ -471,6 +556,13 @@ export default {
       btn_eliminar: false,
       btn_select: false,
       btn_limpiar: true,
+
+      carnetAPDF: {
+        numero_socio: null,
+        afiliado: null,
+        dni: 0,
+        adhrentes: []
+      },
     };
   },
 
@@ -481,12 +573,20 @@ export default {
     id() {
       return this.tabla_socios.numero_socio;
     },
-    items() {
-      return tabla_socios.filter((item) => {
-        return item.numero_socio
-          .toLowerCase()
-          .includes(this.buscar.toLowerCase());
-      });
+    sortOptions() {
+      // Create an options list from our fields
+      return this.fields
+        .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+    },
+
+    //Funcion de busqueda
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
     },
   },
   
@@ -749,6 +849,33 @@ export default {
     },
 
     GenerarPagoAfiliacion() {},
+
+    //Funcion para crear el PDF
+    async generarCarnet(item) {
+
+      let data_socio={};
+      data_socio.numero_socio = item.numero_socio;
+      data_socio.afiliado = item.apellido+', '+item.nombre;
+      data_socio.dni = item.dni;
+      data_socio.adhrentes=[];
+      for(var i=0; i<this.data.length; i++){
+         if(this.data[i].numero_socio.split('/')[4]==data_socio.numero_socio){
+            let data_fam={}
+            data_fam.dni=this.data[i].dni_familiar
+            data_fam.adherente=this.data[i].apellido +', ' + this.data[i].nombre 
+            data_socio.adhrentes.push(data_fam)
+        } 
+      }
+      
+      //this.carnetAPDF = {...item};
+      this.carnetAPDF=data_socio;
+     
+      
+         
+      
+        
+      this.$refs.html2Pdf.generatePdf();
+    },
    
   },
   beforeMount(){
