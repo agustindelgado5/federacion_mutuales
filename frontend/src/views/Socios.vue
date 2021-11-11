1f1b80c2a51005ac7775cce1c29076c0c580afc9
Guido <gldarpino@yahoo.com.ar>
2021-11-10 23:57:36 -0300

    Traslado socio baja mora a archivo aparte

--- frontend/src/views/Socios.vue
+++ frontend/src/views/Socios.vue
@@ -740,132 +740,9 @@
 				
 			
 
+<socios-baja-mora :SocioBajaMora="SocioBajaMora" />
 
-			 <vue-html2pdf
-        :show-layout="true"
-        :float-layout="false"
-        :enable-download="false"
-        :preview-modal="true"
-        :paginate-elements-by-height="1400"
-        filename="Solicitud_Baja"
-        :pdf-quality="2"
-        :manual-pagination="false"
-        pdf-format="a4"
-        pdf-orientation="portrait"  
-        pdf-content-width="95%"
- 
-        @progress="onProgress($event)"
-        @startPagination="startPagination()"
-        @hasPaginated="hasPaginated()"
-        @beforeDownload="beforeDownload($event)"
-        @hasDownloaded="hasDownloaded($event)" 
-        ref="html2PdfBaja"
-    >
-      <section class="ml-5 mt-5 py-3" slot="pdf-content">
-			<p class="text-right">San Miguel de Tucumán {{nota.fecha}}</p>
-			<div class="mt-5">
-				<p>Sr. Ricardo Mora</p>
-				<p>S______/_______D</p>
-			</div>
-			<div class="mt-5">
-				<p class="text-justify" style="text-indent:40%">Quien suscribe {{SocioBajaMora.nombre}} {{SocioBajaMora.apellido}},
-					DNI {{SocioBajaMora.dni}} Nº de afiliado {{SocioBajaMora.numero_socio}} al servicio brindado por Ricardo Mora, quien por medio de la presente
-					vengo a solicitar en forma unilateral la cancelación del servicio de salud y sepelio.
-				</p>
-				<p>Lo cual solicito que el descuento que se hace a través de mi cobro quede cancelado.</p>
-				<p>Autorizo a {{nota.nombre}}, con DNI {{nota.dni}} a PRESENTAR esta nota.</p>
-			</div>
-			<p class="text-right">Sin otro particular saluda atte.</p>
-      </section>
-    </vue-html2pdf>
 
-	<b-form-group label-cols="auto" label="Fecha" label-for="fecha">
-            <b-form-input
-              id="fecha"
-              v-model="nota.fecha"
-              type="text"
-              placeholder="Ingrese una fecha"
-              invalid-feedback="Complete este campo"
-              required
-              >
-			</b-form-input>
-	</b-form-group>
-
-	<b-form-group label-cols="auto" label="N° Socio" label-for="numero_socio">
-            <b-form-input
-              id="numero_socio"
-              v-model="SocioBajaMora.numero_socio"
-              type="number"
-              placeholder="Ingrese un Numero"
-              invalid-feedback="Complete este campo"
-              required
-              >
-			</b-form-input>
-	</b-form-group>
-
-	<b-form-group label-cols="auto" label="Nombre/s" label-for="nombre">
-              <b-form-input
-                id="nombre"
-                v-model="SocioBajaMora.nombre"
-                type="text"
-                placeholder="*Ingrese los Nombre/s"
-                invalid-feedback="Complete este campo"
-                required
-              >
-              </b-form-input>
-            </b-form-group>
-		<b-form-group label-cols="auto" label="Apellido/s" label-for="apellido">
-			<b-form-input
-			id="apellido"
-			v-model="SocioBajaMora.apellido"
-			type="text"
-			placeholder="*Ingrese los Apellido/s"
-			invalid-feedback="Complete este campo"
-			required
-			>
-			</b-form-input>
-		</b-form-group>
-          <b-form-group label-cols="auto" label="DNI" label-for="dni">
-            <b-form-input
-              id="dni"
-              v-model="SocioBajaMora.dni"
-              type="number"
-              placeholder="Ingrese un DNI"
-              invalid-feedback="Complete este campo"
-              required
-            >
-            </b-form-input>
-          </b-form-group>
-
-		  <b-form-group label-cols="auto" label="Nombre que Autoriza" label-for="nombre_nota">
-            <b-form-input
-              id="nombre_nota"
-              v-model="nota.nombre"
-              type="text"
-              placeholder="Ingrese un nombre"
-              invalid-feedback="Complete este campo"
-              required
-            >
-            </b-form-input>
-          </b-form-group>
-
-		  <b-form-group label-cols="auto" label="DNI que Autoriza" label-for="dni_nota">
-            <b-form-input
-              id="dni_nota"
-              v-model="nota.dni"
-              type="text"
-              placeholder="Ingrese un DNI"
-              invalid-feedback="Complete este campo"
-              required
-            >
-            </b-form-input>
-          </b-form-group>
-		  <b-button
-				@click="generarPDFSolicitudBaja()"
-				class="mb-4 ml-2"
-				title="PDF de la baja"
-				>Generar PDF</b-button
-			>
 	
 </b-modal>
 
@@ -1254,14 +1131,13 @@
 			},
 
 			async generarSolicitudBaja(item) {
-				this.SocioBajaMora={...item};
-				this.fecha=new Date().toLocaleDateString('es-ar',{month:'long',day:'numeric',year:'numeric'});
+				this.SocioBajaMora={...item};				
 				this.$refs["modal-bajamora"].show();
 			},
 
-			async generarPDFSolicitudBaja(){
-				this.$refs.html2PdfBaja.generatePdf();
-			}
+			// async generarPDFSolicitudBaja(){
+			// 	this.$refs.html2PdfBaja.generatePdf();
+			// }
 
 		},
 		beforeMount() {
--- frontend/src/views/SociosBajaMora.vue
+++ frontend/src/views/SociosBajaMora.vue
@@ -1,7 +1,8 @@
 <template>
+  <div>
     <vue-html2pdf
-        :show-layout="false"
-        :float-layout="true"
+        :show-layout="true"
+        :float-layout="false"
         :enable-download="false"
         :preview-modal="true"
         :paginate-elements-by-height="1400"
@@ -10,8 +11,7 @@
         :manual-pagination="false"
         pdf-format="a4"
         pdf-orientation="portrait"  
-        pdf-content-width="80%"
-        @click="generarSolicitud()"
+        pdf-content-width="95%"
  
         @progress="onProgress($event)"
         @startPagination="startPagination()"
@@ -20,31 +20,136 @@
         @hasDownloaded="hasDownloaded($event)" 
         ref="html2PdfBaja"
     >
-      <section slot="pdf-content">
-        <!-- PDF Content Here -->
-          <section class="pdf-item">
-            <img src="../assets/logo.jpg" alt="Logo Federación" srcset="" id="Logo_fed" >
-          </section>
-          <section class="pdf-item">
-              Viendo que puedo poner je
-          </section>
+      <section class="ml-5 mt-5 py-3" slot="pdf-content">
+			<p class="text-right">San Miguel de Tucumán {{nota.fecha}}</p>
+			<div class="mt-5">
+				<p>Sr. Ricardo Mora</p>
+				<p>S______/_______D</p>
+			</div>
+			<div class="mt-5">
+				<p class="text-justify" style="text-indent:40%">Quien suscribe {{SocioBajaMora.nombre}} {{SocioBajaMora.apellido}},
+					DNI {{SocioBajaMora.dni}} Nº de afiliado {{SocioBajaMora.numero_socio}} al servicio brindado por Ricardo Mora, quien por medio de la presente
+					vengo a solicitar en forma unilateral la cancelación del servicio de salud y sepelio.
+				</p>
+				<p>Lo cual solicito que el descuento que se hace a través de mi cobro quede cancelado.</p>
+				<p>Autorizo a {{nota.nombre}}, con DNI {{nota.dni}} a PRESENTAR esta nota.</p>
+			</div>
+			<p class="text-right">Sin otro particular saluda atte.</p>
       </section>
     </vue-html2pdf>
-  
+
+	<b-form-group label-cols="auto" label="Fecha" label-for="fecha">
+            <b-form-input
+              id="fecha"
+              v-model="nota.fecha"
+              type="text"
+              placeholder="Ingrese una fecha"
+              invalid-feedback="Complete este campo"
+              required
+              >
+			</b-form-input>
+	</b-form-group>
+
+	<b-form-group label-cols="auto" label="N° Socio" label-for="numero_socio">
+            <b-form-input
+              id="numero_socio"
+              v-model="SocioBajaMora.numero_socio"
+              type="number"
+              placeholder="Ingrese un Numero"
+              invalid-feedback="Complete este campo"
+              required
+              >
+			</b-form-input>
+	</b-form-group>
+
+	<b-form-group label-cols="auto" label="Nombre/s" label-for="nombre">
+              <b-form-input
+                id="nombre"
+                v-model="SocioBajaMora.nombre"
+                type="text"
+                placeholder="*Ingrese los Nombre/s"
+                invalid-feedback="Complete este campo"
+                required
+              >
+              </b-form-input>
+            </b-form-group>
+		<b-form-group label-cols="auto" label="Apellido/s" label-for="apellido">
+			<b-form-input
+			id="apellido"
+			v-model="SocioBajaMora.apellido"
+			type="text"
+			placeholder="*Ingrese los Apellido/s"
+			invalid-feedback="Complete este campo"
+			required
+			>
+			</b-form-input>
+		</b-form-group>
+          <b-form-group label-cols="auto" label="DNI" label-for="dni">
+            <b-form-input
+              id="dni"
+              v-model="SocioBajaMora.dni"
+              type="number"
+              placeholder="Ingrese un DNI"
+              invalid-feedback="Complete este campo"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
+
+		  <b-form-group label-cols="auto" label="Nombre que Autoriza" label-for="nombre_nota">
+            <b-form-input
+              id="nombre_nota"
+              v-model="nota.nombre"
+              type="text"
+              placeholder="Ingrese un nombre"
+              invalid-feedback="Complete este campo"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
+
+		  <b-form-group label-cols="auto" label="DNI que Autoriza" label-for="dni_nota">
+            <b-form-input
+              id="dni_nota"
+              v-model="nota.dni"
+              type="text"
+              placeholder="Ingrese un DNI"
+              invalid-feedback="Complete este campo"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
+		  <b-button
+				@click="generarPDFSolicitudBaja()"
+				class="mb-4 ml-2"
+				title="PDF de la baja"
+				>Generar PDF</b-button
+			>
+	</div>
 </template>
 
 <script>
 export default {
+  
     props:{
-        datos:{},
+        SocioBajaMora:{},
     },
 
     data(){
-        return {}
+        
+        return {
+
+        nota:{
+					fecha:null,
+					nombre: "........",
+					dni: "........",
+				},
+
+        }
     },
 
     methods: {
-        generarSolicitud(){
+        async generarSolicitud(){
             if(false || this.datos !=0){
                 this.$refs.html2PdfBaja.generatePdf();
             }
@@ -52,6 +157,9 @@ export default {
                 swal("Debe tener al menos 1 registro")
             }
         },
+    },
+    beforeMount (){
+      this.nota.fecha=new Date().toLocaleDateString('es-ar',{month:'long',day:'numeric',year:'numeric'});
     }
 
 
