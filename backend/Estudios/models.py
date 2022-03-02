71dabdcca2be8382245323255e5c4c48a5863e16
Leonel-H29 <leonelherreradkno@gmail.com>
2022-03-02 15:10:07 -0300

    Agrego la entidad para los proveedores

--- backend/Estudios/models.py
+++ backend/Estudios/models.py
@@ -1,9 +1,53 @@
 from django.db import models
+from backend.deptos import provincias
 from backend.tipos_Estudios import estudios_tipos
 from django.db.models.fields import AutoField
 from Socios.models import socios
 
 # Create your models here.
+
+"""
+Construyo la entidad del Proveedor para proveer un estudio
+"""
+
+
+class proveedor(models.Model):
+    id_proveedor = AutoField(primary_key=True)
+    #Datos personales
+    nombre = models.CharField(max_length=100)
+    cuit = models.CharField(unique=True, max_length=11)
+
+    #Direccion particular
+    direccion = models.CharField(max_length=50)
+    localidad = models.CharField(max_length=30)
+    provincia = models.CharField(max_length=30, choices=provincias)
+
+    #Contacto
+    email = models.EmailField(null=True, blank=True)
+    tel_fijo = models.IntegerField(null=True, blank=True)
+    tel_celular = models.IntegerField(null=True, blank=True)
+
+
+    #Datos en el sistema
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "proveedores"
+        verbose_name = "proveedor"
+        verbose_name_plural = "proveedoreses"
+        ordering = ["id_proveedor"]
+
+    def __str__(self):
+        cadena = (
+            str(self.id_proveedor)
+            + "-"
+            + self.nombre
+            + "-"
+            + str(self.cuit)
+        )
+        return cadena
+
 
 """
 Construyo la entidad para estudios, éstos pueden ser de análisis bioquimico, diagnóstico
@@ -19,8 +63,7 @@ class estudios(models.Model):
     nbu = models.IntegerField(null=True, blank=True)
     descripcion=models.CharField(max_length=60)
     denominación=models.CharField(null=True,max_length=30)
-
-    proveedor = models.CharField(null=True, blank=True, max_length=40)
+    id_proveedor = models.ForeignKey(proveedor, models.DO_NOTHING)
     precio_socio = models.DecimalField(max_digits=8, decimal_places=2)
     precio_federacion = models.DecimalField(max_digits=8, decimal_places=2)
     
---
+++ frontend/src/views/proveedor/ProveedoresUpdate.vue
@@ -0,0 +1,277 @@
+<template>
+	<div>
+		<h6>Los campos en (*) son obligatorios</h6>
+
+		<b-form>
+			<b-card no-body class="mb-1">
+				<b-card-header header-tag="header" class="p-1" role="tab">
+					<b-button
+						block
+						v-b-toggle.accordion-1
+						style="background-color: darkorange"
+						>Datos Principales:</b-button
+					>
+				</b-card-header>
+				<b-collapse
+					id="accordion-1"
+					visible
+					accordion="my-accordion"
+					role="tabpanel"
+				>
+					<b-card-body>
+						<b-form-group label="*Nombre" label-for="nombre">
+							<b-form-input
+								id="nombre"
+								v-model="proveedor.nombre"
+								type="text"
+								placeholder="Ingrese un nombre"
+								invalid-feedback="Complete este campo"
+								:state="validacion.nombre.estado"
+								required
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="nombre-live-feedback">
+								{{ validacion.nombre.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+
+						<b-form-group label="*CUIT" label-for="cuit">
+							<b-form-input
+								id="cuit"
+								v-model="proveedor.cuit"
+								:state="validacion.cuit.estado"
+								type="number"
+								placeholder="Ingrese un cuit"
+								invalid-feedback="Complete este campo"
+								required
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="cuit-live-feedback">
+								{{ validacion.cuit.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+					</b-card-body>
+				</b-collapse>
+			</b-card>
+			<b-card no-body class="mb-1">
+				<b-card-header header-tag="header" class="p-1" role="tab">
+					<b-button
+						block
+						v-b-toggle.accordion-2
+						style="background-color: darkorange"
+						>Direccion:</b-button
+					>
+				</b-card-header>
+				<b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+					<b-card-body>
+						<b-form-group label="*Calle" label-for="direccion">
+							<b-form-input
+								id="direccion"
+								v-model="proveedor.direccion"
+								:state="validacion.direccion.estado"
+								type="text"
+								placeholder="Ingrese una calle"
+								invalid-feedback="Complete este campo"
+								required
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="direccion-live-feedback"
+								>{{ validacion.direccion.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+						<b-form-group label="*Localidad" label-for="localidad">
+							<b-form-input
+								id="localidad"
+								v-model="proveedor.localidad"
+								:state="validacion.localidad.estado"
+								type="text"
+								placeholder="Ingrese una localidad"
+								invalid-feedback="Complete este campo"
+								required
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="localidad-live-feedback"
+								>{{ validacion.localidad.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+						<b-form-group label="*Provincia" label-for="provincia">
+							<b-form-select
+								id="provincia"
+								v-model="proveedor.provincia"
+								:state="validacion.provincia.estado"
+								type="text"
+								placeholder="Ingrese una provincia"
+								invalid-feedback="Complete este campo"
+								required
+								:options="options"
+							>
+							</b-form-select>
+						</b-form-group>
+					</b-card-body>
+				</b-collapse>
+			</b-card>
+
+			<b-card no-body class="mb-1">
+				<b-card-header header-tag="header" class="p-1" role="tab">
+					<b-button
+						block
+						v-b-toggle.accordion-3
+						style="background-color: darkorange"
+						>Contacto:</b-button
+					>
+				</b-card-header>
+				<b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+					<b-card-body>
+						<b-form-group label="Email" label-for="email">
+							<b-form-input
+								id="email"
+								v-model="proveedor.email"
+								:state="validacion.email.estado"
+								type="email"
+								placeholder="Ingrese un email"
+								invalid-feedback="Complete este campo"
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="email-live-feedback"
+								>{{ validacion.email.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+
+						<b-form-group label="Telefono fijo" label-for="tel_fijo">
+							<b-form-input
+								id="tel_fijo"
+								v-model="proveedor.tel_fijo"
+								:state="validacion.tel_fijo.estado"
+								type="number"
+								placeholder="Ingrese un numero"
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="tel_fijo-live-feedback"
+								>{{ validacion.tel_fijo.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+
+						<b-form-group label="Celular" label-for="tel_celular">
+							<b-form-input
+								id="tel_celular"
+								v-model="proveedor.tel_celular"
+								:state="validacion.tel_celular.estado"
+								type="number"
+								placeholder="Ingrese un numero"
+							>
+							</b-form-input>
+							<b-form-invalid-feedback id="tel_celular-live-feedback"
+								>{{ validacion.tel_celular.mensaje }}
+							</b-form-invalid-feedback>
+						</b-form-group>
+					</b-card-body>
+				</b-collapse>
+			</b-card>
+		</b-form>
+		<b-button
+			class="mt-2"
+			variant="success"
+			block
+			@click.prevent="putproveedor()"
+			>Guardar</b-button
+		>
+	</div>
+</template>
+
+<script>
+	import { APIControler } from "@/store/APIControler";
+	import axios from "axios";
+
+	export default {
+		props: {
+			proveedor: {},
+
+			updateTable: Function,
+		},
+		data() {
+			return {
+				respuesta: {},
+				validarError: {
+					validateStatus: function (status) {
+						return status < 500; // Resolve only if the status code is less than 500
+					},
+				},
+				validacion: {
+					id_proveedor: { estado: null, mensaje: "" },
+					nombre: { estado: null, mensaje: "" },
+					cuit: { estado: null, mensaje: "" },
+					direccion: { estado: null, mensaje: "" },
+					provincia: { estado: null, mensaje: "" },
+					localidad: { estado: null, mensaje: "" },
+					email: { estado: null, mensaje: "" },
+					tel_fijo: { estado: null, mensaje: "" },
+					tel_celular: { estado: null, mensaje: "" },
+				},
+				options: [
+					{ value: "Buenos Aires", text: "1- Buenos Aires" },
+					{ value: "Catamarca", text: "2- Catamarca" },
+					{ value: "Chaco", text: "3- Chaco" },
+					{ value: "Chubut", text: "4- Chubut" },
+					{ value: "Córdoba", text: "5- Córdoba" },
+					{ value: "Corrientes", text: "6- Corrientes" },
+					{ value: "Entre Ríos", text: "7- Entre Ríos" },
+					{ value: "Formosa", text: "8- Formosa" },
+					{ value: "Jujuy", text: "9- Jujuy" },
+					{ value: "La Pampa", text: "10- La Pampa" },
+					{ value: "La Rioja", text: "11- La Rioja" },
+					{ value: "Mendoza", text: "12- Mendoza" },
+					{ value: "Misiones", text: "13- Misiones" },
+					{ value: "Neuquén", text: "14- Neuquén" },
+					{ value: "Río Negro", text: "15- Río Negro" },
+					{ value: "Salta", text: "16- Salta" },
+					{ value: "San Juan", text: "17- San Juan" },
+					{ value: "San Luis", text: "18- San Luis" },
+					{ value: "Santa Cruz", text: "19- Santa Cruz" },
+					{ value: "Santa Fe", text: "20- Santa Fe" },
+					{ value: "Santiago del Estero", text: "21- Santiago del Estero" },
+					{ value: "Tierra del Fuego", text: "22- Tierra del Fuego" },
+					{ value: "Tucumán", text: "23- Tucumán" },
+				],
+			};
+		},
+		methods: {
+			async putproveedor() {
+				try {
+					this.respuesta = await axios.put(
+						"http://localhost:8081/proveedor_estudios/" +
+							this.proveedor.id_proveedor +
+							"/",
+						this.proveedor,
+						this.validarError
+					);
+					const content = this.respuesta.request;
+					if (content.statusText == "OK") {
+						this.respuesta = "";
+						swal("Carga Exitosa", " ", "success");
+					} else {
+						//console.log(content.response);
+						this.respuesta = JSON.parse(content.response);
+						swal("¡ERROR!", "Los datos no son válidos", "error");
+					}
+				} catch (error) {
+					swal("¡ERROR!", "Se ha detectado un problema ", "error");
+					console.log(error.toJSON());
+				}
+				this.cargarFeedback();
+				this.updateTable();
+			},
+
+			cargarFeedback() {
+				let valido;
+				for (let key in this.validacion) {
+					valido = !this.respuesta.hasOwnProperty(key);
+					this.validacion[key].estado = valido;
+					//console.log(key);
+					if (!valido) this.validacion[key].mensaje = this.respuesta[key][0];
+				}
+			},
+		},
+	};
+</script>
+
+<style></style>
