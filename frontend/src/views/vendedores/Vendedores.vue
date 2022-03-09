f00174a6901f47661a4bf6e999b5fab7c06371e1
Leonel-H29 <leonelherreradkno@gmail.com>
2022-03-09 07:14:15 -0300

    Vendedores con sus filtros correspondientes

--- frontend/src/views/vendedores/Vendedores.vue
+++ frontend/src/views/vendedores/Vendedores.vue
@@ -147,7 +147,11 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 					sortable
 					responsive
 					hover
-					:items="tabla_vendedores"
+					:items="
+						tabla_vendedores
+							| Localidad(filter_localidad)
+							| Departamento(filter_departamento)
+					"
 					show-empty
 					:sticky-header="true"
 					:no-border-collapse="false"
@@ -180,7 +184,7 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 					</template>
 
 					<template slot="cell(fecha_nacimiento)" slot-scope="data">
-						{{ data.value | Date}}
+						{{ data.value | Date }}
 					</template>
 
 					<template #cell(selected)="{ rowSelected }">
@@ -341,7 +345,6 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 					</b-card-group>
 				</div>
 				<br />
-
 				<div>
 					<b-card-group deck>
 						<b-card
@@ -355,32 +358,70 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 									<b-card-header header-tag="header" class="p-1" role="tab">
 										<b-button
 											block
-											v-b-toggle.accordion-1
+											v-b-toggle.accordion-filter-1
 											variant="info"
 											style="font-size: 0.82em"
 										>
-											SOCIO
+											DEPARTAMENTO
 										</b-button>
 									</b-card-header>
 									<b-collapse
-										id="accordion-1"
+										id="accordion-filter-1"
 										visible
 										accordion="my-accordion"
 										role="tabpanel"
 									>
 										<b-card-body>
-											<b-form-group id="input-group-1">
+											<b-form-group id="input-group-4">
 												<v-autocomplete
-													id="socio"
-													v-model="filter_socio"
-													:items="options_socio"
+													id="departamento"
+													v-model="filter_departamento"
+													:items="options_deptos"
 													type="text"
 													solo
 													filled
 												></v-autocomplete>
-												<div v-show="filter_socio != null">
+												<div v-show="filter_departamento != null">
 													<b-button
-														@click="filter_socio = null"
+														@click="filter_departamento = null"
+														title="Limpiar"
+													>
+														Limpiar
+													</b-button>
+												</div>
+											</b-form-group>
+										</b-card-body>
+									</b-collapse>
+
+									<b-card-header header-tag="header" class="p-1" role="tab">
+										<b-button
+											block
+											v-b-toggle.accordion-filter-2
+											variant="info"
+											style="font-size: 0.82em"
+										>
+											LOCALIDAD
+										</b-button>
+									</b-card-header>
+									<b-collapse
+										id="accordion-filter-2"
+										visible
+										accordion="my-accordion"
+										role="tabpanel"
+									>
+										<b-card-body>
+											<b-form-group id="input-group-4">
+												<v-autocomplete
+													id="localidad"
+													v-model="filter_localidad"
+													:items="op_localidad"
+													type="text"
+													solo
+													filled
+												></v-autocomplete>
+												<div v-show="filter_localidad != null">
+													<b-button
+														@click="filter_localidad = null"
 														title="Limpiar"
 													>
 														Limpiar
@@ -394,7 +435,6 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 						</b-card>
 					</b-card-group>
 				</div>
-				-->
 			</aside>
 
 			<b-modal id="modal-editar" hide-footer>
@@ -474,13 +514,17 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 					{ key: "apellido", label: "Apellido", sortable: true },
 					{ key: "nombre", label: "Nombre", sortable: true },
 					{ key: "dni", label: "DNI", sortable: true },
-					{ key: "fecha_nacimiento", label: "Fecha Nacimiento", sortable: true },
+					{
+						key: "fecha_nacimiento",
+						label: "Fecha Nacimiento",
+						sortable: true,
+					},
 					{ key: "calle", label: "Calle", sortable: true },
 					{ key: "localidad", label: "Localidad", sortable: true },
 					{ key: "departamento", label: "Departamento", sortable: true },
-                    { key: "cod_postal", label: "Codigo Postal", sortable: true },
-                    { key: "tel_fijo", label: "Telefono Fijo", sortable: true },
-                    { key: "tel_celular", label: "Telefono Celular", sortable: true },
+					{ key: "cod_postal", label: "Codigo Postal", sortable: true },
+					{ key: "tel_fijo", label: "Telefono Fijo", sortable: true },
+					{ key: "tel_celular", label: "Telefono Celular", sortable: true },
 					{ key: "action", label: "Acciones", variant: "secondary" },
 				],
 				filter: null,
@@ -506,11 +550,231 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 				btn_eliminar: false,
 				btn_select: false,
 				//Campos a filtrar
-				filter_socio: null,
+				filter_localidad: null,
+				filter_departamento: null,
 
 				//Opciones de filtrado
-				options_socio: [
-					{ value: null, text: "Elija un socio", selected: true },
+				options_deptos: [
+					{
+						value: null,
+						text: "Elija un departamento",
+						selected: true,
+					},
+					{ value: "Burruyacú", text: "1- Burruyacú" },
+					{ value: "Capital", text: "2- Capital" },
+					{ value: "Chicligasta", text: "3- Chicligasta" },
+					{ value: "Cruz Alta", text: "4- Cruz Alta" },
+					{ value: "Famaillá", text: "5- Famaillá" },
+					{ value: "Graneros", text: "6- Graneros" },
+					{ value: "Juan Bautista Alberdi", text: "7- Juan Bautista Alberdi" },
+					{ value: "La Cocha", text: "8- La Cocha" },
+					{ value: "Leales", text: "9- Leales" },
+					{ value: "Lules", text: "10- Lules" },
+					{ value: "Monteros", text: "11- Monteros" },
+					{ value: "Río Chico", text: "12- Río Chico" },
+					{ value: "Simoca", text: "13- Simoca" },
+					{ value: "Tafí del Valle", text: "14- Tafí del Valle" },
+					{ value: "Tafí Viejo", text: "15- Tafí Viejo" },
+					{ value: "Trancas", text: "16- Trancas" },
+					{ value: "Yerba Buena", text: "17- Yerba Buena" },
+				],
+				op_localidad: [
+					{ value: null, text: "Elija una localidad" },
+					{ value: "Acheral", text: " 1 - Acheral " },
+					{
+						value: "Agua Dulce y La Soledad",
+						text: "2 - Agua Dulce y La Soledad",
+					},
+					{ value: "Aguilares", text: " 3 - Aguilares " },
+					{ value: "Alderetes", text: " 4 - Alderetes " },
+					{
+						value: "Alpachiri y El Molino",
+						text: " 5 - Alpachiri y El Molino ",
+					},
+					{
+						value: "Alto Verde y Los Gucheas",
+						text: " 6 - Alto Verde y Los Gucheas ",
+					},
+					{ value: "Amaichá del Valle", text: " 7 - Amaichá del Valle " },
+					{ value: "Amberes", text: " 8 - Amberes " },
+					{ value: "Anca Juli", text: " 9 - Anca Juli " },
+					{ value: "Arcadia", text: " 10 - Arcadia " },
+					{ value: "Atahona", text: " 11 - Atahona " },
+					{ value: "Banda del Río Salí", text: " 12 - Banda del Río Salí " },
+					{ value: "Bella Vista", text: " 13 - Bella Vista " },
+					{ value: "Buena Vista", text: " 14 - Buena Vista " },
+					{ value: "Burruyacú", text: " 15 - Burruyacú " },
+					{ value: "Capitán Cáceres", text: " 16 - Capitán Cáceres " },
+					{ value: "Cevil Redondo", text: " 17 - Cevil Redondo " },
+					{ value: "Choromoro", text: " 18 - Choromoro " },
+					{ value: "Ciudacita", text: " 19 - Ciudacita " },
+					{ value: "Colalao del Valle", text: " 20 - Colalao del Valle " },
+					{ value: "Colombres", text: " 21 - Colombres " },
+					{ value: "Concepción", text: " 22 - Concepción " },
+					{
+						value: "Delfín Gallo (Ex Ingenio Esperanza)",
+						text: " 23 - Delfín Gallo (Ex Ingenio Esperanza) ",
+					},
+					{
+						value: "El Bracho y El Cevilar",
+						text: " 24 - El Bracho y El Cevilar ",
+					},
+					{ value: "El Cadillal", text: " 25 - El Cadillal " },
+					{ value: "El Cercado", text: " 26 - El Cercado " },
+					{ value: "El Chañar", text: " 27 - El Chañar " },
+					{ value: "El Manantial", text: " 28 - El Manantial " },
+					{ value: "El Mojón", text: " 29 - El Mojón " },
+					{ value: "El Mollar", text: " 30 - El Mollar " },
+					{ value: "El Naranjito", text: " 31 - El Naranjito " },
+					{
+						value: "El Naranjo y El Sunchal",
+						text: " 32 - El Naranjo y El Sunchal ",
+					},
+					{ value: "El Polear", text: " 33 - El Polear " },
+					{ value: "El Puestito", text: " 34 - El Puestito " },
+					{ value: "El Sacrificio", text: " 35 - El Sacrificio " },
+					{ value: "El Timbó", text: " 36 - El Timbó " },
+					{ value: "Escaba", text: " 37 - Escaba " },
+					{ value: "Esquina y Mancopa", text: " 38 - Esquina y Mancopa " },
+					{
+						value: "Estación Araox y Tacanas",
+						text: " 39 - Estación Araox y Tacanas ",
+					},
+					{ value: "Famaillá", text: " 40 - Famaillá " },
+					{ value: "Gastona y Belicha", text: " 41 - Gastona y Belicha " },
+					{
+						value: "Gobernador Garmendia",
+						text: " 42 - Gobernador Garmendia ",
+					},
+					{
+						value: "Gobernador Piedrabuena",
+						text: " 43 - Gobernador Piedrabuena ",
+					},
+					{ value: "Graneros", text: " 44 - Graneros " },
+					{ value: "Huasa Pampa", text: " 45 - Huasa Pampa " },
+					{
+						value: "Juan Bautista Alberdi",
+						text: " 46 - Juan Bautista Alberdi ",
+					},
+					{ value: "La Cocha", text: " 47 - La Cocha " },
+					{ value: "La Esperanza", text: " 48 - La Esperanza " },
+					{
+						value: "La Florida y Luisiana",
+						text: " 49 - La Florida y Luisiana ",
+					},
+					{
+						value: "La Ramada y La Cruz",
+						text: " 50 - La Ramada y La Cruz ",
+					},
+					{ value: "La Trinidad", text: " 51 - La Trinidad " },
+					{ value: "Lamadrid", text: " 52 - Lamadrid " },
+					{ value: "Las Cejas", text: " 53 - Las Cejas " },
+					{ value: "Las Talas", text: " 54 - Las Talas " },
+					{ value: "Las Talitas", text: " 55 - Las Talitas " },
+					{
+						value: "Los Bulacio y Los Villagra",
+						text: " 56 - Los Bulacio y Los Villagra ",
+					},
+					{ value: "Los Gómez", text: " 57 - Los Gómez " },
+					{ value: "Los Nogales", text: " 58 - Los Nogales " },
+					{ value: "Los Pereyra", text: " 59 - Los Pereyra " },
+					{ value: "Los Puestos", text: " 60 - Los Puestos " },
+					{ value: "Los Pérez", text: " 61 - Los Pérez " },
+					{ value: "Los Ralos", text: " 62 - Los Ralos " },
+					{
+						value: "Los Sarmientos y La Tipa",
+						text: " 63 - Los Sarmientos y La Tipa ",
+					},
+					{ value: "Los Sosas", text: " 64 - Los Sosas " },
+					{ value: "Lules", text: " 65 - Lules " },
+					{
+						value: "Manuel García Fernández",
+						text: " 66 - Manuel García Fernández ",
+					},
+					{ value: "Manuela Pedraza", text: " 67 - Manuela Pedraza " },
+					{ value: "Medinas", text: " 68 - Medinas " },
+					{ value: "Monte Bello", text: " 69 - Monte Bello " },
+					{ value: "Monteagudo", text: " 70 - Monteagudo " },
+					{ value: "Monteros", text: " 71 - Monteros " },
+					{ value: "Padre Monti", text: " 72 - Padre Monti " },
+					{ value: "Pampa Mayo", text: " 73 - Pampa Mayo " },
+					{
+						value: "Quilmes y Los Sueldos",
+						text: " 74 - Quilmes y Los Sueldos ",
+					},
+					{ value: "Raco", text: " 75 - Raco " },
+					{
+						value: "Ranchillos y San Miguel",
+						text: " 76 - Ranchillos y San Miguel ",
+					},
+					{ value: "Rumi Punco", text: " 77 - Rumi Punco " },
+					{
+						value: "Río Chico y Nueva Trinidad",
+						text: " 78 - Río Chico y Nueva Trinidad ",
+					},
+					{ value: "Río Colorado", text: " 79 - Río Colorado " },
+					{ value: "Río Seco", text: " 80 - Río Seco " },
+					{ value: "San Andrés", text: " 81 - San Andrés " },
+					{
+						value: "San Felipe y Santa Bárbara",
+						text: " 82 - San Felipe y Santa Bárbara ",
+					},
+					{ value: "San Ignacio", text: " 83 - San Ignacio " },
+					{ value: "San Javier", text: " 84 - San Javier " },
+					{
+						value: "San José de la Cocha",
+						text: " 85 - San José de la Cocha ",
+					},
+					{
+						value: "San Miguel de Tucumán",
+						text: " 86 - San Miguel de Tucumán ",
+					},
+					{
+						value: "San Pablo y Villa Nougués",
+						text: " 87 - San Pablo y Villa Nougués ",
+					},
+					{
+						value: "San Pedro de Colalao",
+						text: " 88 - San Pedro de Colalao ",
+					},
+					{
+						value: "San Pedro y San Antonio",
+						text: " 89 - San Pedro y San Antonio ",
+					},
+					{ value: "Santa Ana", text: " 90 - Santa Ana " },
+					{
+						value: "Santa Cruz y La Tuna",
+						text: " 91 - Santa Cruz y La Tuna ",
+					},
+					{ value: "Santa Lucía", text: " 92 - Santa Lucía " },
+					{
+						value: "Santa Rosa de Leales",
+						text: " 93 - Santa Rosa de Leales ",
+					},
+					{
+						value: "Santa Rosa y Los Rojo",
+						text: " 94 - Santa Rosa y Los Rojo ",
+					},
+					{ value: "Sargento Moya", text: " 95 - Sargento Moya " },
+					{ value: "Siete de Abril", text: " 96 - Siete de Abril " },
+					{ value: "Simoca", text: " 97 - Simoca " },
+					{ value: "Soldado Maldonado", text: " 98 - Soldado Maldonado " },
+					{ value: "Taco Ralo", text: " 99 - Taco Ralo " },
+					{ value: "Tafí Viejo", text: " 100 - Tafí Viejo " },
+					{ value: "Tafí del Valle", text: " 101 - Tafí del Valle " },
+					{ value: "Tapia", text: " 102 - Tapia " },
+					{ value: "Teniente Berdina", text: " 103 - Teniente Berdina " },
+					{ value: "Trancas", text: " 104 - Trancas " },
+					{ value: "Villa Belgrano", text: " 105 - Villa Belgrano " },
+					{
+						value: "Villa Benjamín Araoz",
+						text: " 106 - Villa Benjamín Araoz ",
+					},
+					{ value: "Villa Chigligasta", text: " 107 - Villa Chigligasta " },
+					{ value: "Villa Quinteros", text: " 108 - Villa Quinteros " },
+					{ value: "Villa de Leales", text: " 109 - Villa de Leales " },
+					{ value: "Yerba Buena", text: " 110 - Yerba Buena " },
+					{ value: "Yánima", text: " 111 - Yánima " },
 				],
 
 				vendedorAPDF: {}, //Se carga cuando se hace clic en exportar a pdf, con el vendedor a exportar
@@ -544,19 +808,6 @@ Cantidad de registros: {{ rows }} | Filas seleccionadas: {{
 					console.log(lista_vendedores);
 
 					this.tabla_vendedores = lista_vendedores;
-
-					this.tabla_vendedores.forEach((element) => {
-						let opcionSoc = {};
-
-						opcionSoc.value = element.numero_socio;
-						opcionSoc.text = element.numero_socio.split("/")[4];
-
-						if (this.options_socio.find((x) => x.value == opcionSoc.value)) {
-							console.log(opcionSoc, " ya se encuentra en el listado");
-						} else {
-							this.options_socio.push(opcionSoc);
-						}
-					});
 				} catch (error) {
 					console.log(error);
 				}
